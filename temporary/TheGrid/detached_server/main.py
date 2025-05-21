7a7a23c82cafa4d1d0d8f8b31814b5771bae6939
wkta <thomas.iw@kata.games>
2025-05-21 14:38:54 +0200

    (docs)add infos to illustrate layout of a detached server

---
+++ temporary/TheGrid/detached_server/main.py
@@ -0,0 +1,20 @@
+# Automatically generated, do not edit!
+# detached server for the bundle "TheGrid"
+# pyved version 25.4a1
+
+
+import os  # to access env variables
+import pyved_engine
+from netcode.shared_code import glvars
+from netcode import run_server
+
+glvars.pyv = pyved_engine.get_engine_router()
+glvars.pyv.server_flag=True
+
+# as long as you dont booststrap you cant use .umediator etc
+glvars.pyv.bootstrap_e()
+
+PORTINFO = os.getenv('PORT')
+HOSTINFO = os.getenv('IP')
+print('starting game server,',PORTINFO,';',HOSTINFO)
+run_server.exec(**{'port':PORTINFO,'host':HOSTINFO,'mode':'ws'})
